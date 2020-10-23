247a96b562caa5ed7d2cd9c8cfc95ccfc9698554
Senseirn <rnznbhrsfz@gmail.com>
2020-10-24 07:49:36 +0900

    re-apply clang-format and add some initial implementation

---
+++ sensy/layers/abstract_layer.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "sensy/backends/backends.h"
+#include "sensy/utils/utils.h"
+
+namespace ssy {
+namespace layers {
+template <typename T>
+class abstract_layer {
+ private:
+  tensor2d<float> _in;
+};
+} // namespace layers
+} // namespace ssy
