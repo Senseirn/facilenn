6678231d02cfa21ce77de71d68fe5ba6860f879b
Yuta Kanbara <kanbara@beyondjapan.com>
2020-11-30 22:40:46 +0900

    ns内部のインデントを有効化

--- sensy/backends/tensor/tensor.h
+++ sensy/backends/tensor/tensor.h
@@ -3,6 +3,6 @@
 #include "sensy/backends/tensor/tensor_core.h"
 
 namespace ssy {
-template <typename T>
-using tensor2d = ts::tensor<T, 2>;
+  template <typename T>
+  using tensor2d = ts::tensor<T, 2>;
 }
