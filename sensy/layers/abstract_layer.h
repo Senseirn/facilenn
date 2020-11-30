ea187bd5bb1205e846317eecb9c6aaa961ce2fbc
Senseirn <rnznbhrsfz@gmail.com>
2020-11-30 22:40:46 +0900

    ns内部のインデントを有効化

--- sensy/layers/abstract_layer.h
+++ sensy/layers/abstract_layer.h
@@ -4,11 +4,11 @@
 #include "sensy/utils/utils.h"
 
 namespace ssy {
-namespace layers {
-template <typename T>
-class abstract_layer {
- private:
-  tensor2d<float> _in;
-};
-} // namespace layers
+  namespace layers {
+    template <typename T>
+    class abstract_layer {
+     private:
+      tensor2d<float> _in;
+    };
+  } // namespace layers
 } // namespace ssy
