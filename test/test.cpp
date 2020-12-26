7dd02eef91311bd44f88239f4ceb2a1ed0aaa564
Senseirn <rnznbhrsfz@gmail.com>
2020-12-27 00:31:39 +0900

    change network interfaces

--- test/test.cpp
+++ test/test.cpp
@@ -6,27 +6,38 @@ int main() {
   using namespace tino::core;
   using namespace tino::utils;
 
+  // define network
   tino::network net;
-  SGD sgd;
-  sgd.alpha(0.2);
-  net.add(new fully_connected_layer(64, 32, sgd()));
+  net.add(new fully_connected_layer(64, 32));
   net.add(new relu_layer());
-  net.add(new fully_connected_layer(32, 16, sgd()));
+  net.add(new fully_connected_layer(32, 16));
   net.add(new relu_layer());
-  net.add(new fully_connected_layer(16, 1, sgd()));
+  net.add(new fully_connected_layer(16, 1));
   net.add(new relu_layer());
   //  net.initialize();
 
-  tensor2d<float> train_inputs(1000, 2);
-  tensor2d<float> train_labels(1000, 1);
-  xor_generator generator;
-  generator.generate(train_inputs, train_labels);
-
-  net.train(train_inputs, train_labels, 30, 20, [](tensor2d<float>& x) {
+  // define how to initialize weights
+  net.weight_initializer([](tensor2d<float>& x) {
     std::random_device rnd;
     std::mt19937 mt(rnd());
     std::uniform_real_distribution<> rand(-0.5, 0.5);
     for (auto& e : x)
       e = rand(mt);
   });
+
+  // declare optimizer
+  // here we use SGD with paramert alpha = 0.2
+  optimizers::SGD sgd;
+  sgd.alpha(0.2);
+
+  tensor2d<float> train_inputs(1000, 2);
+  tensor2d<float> train_labels(1000, 1);
+  xor_generator generator;
+  generator.generate(train_inputs, train_labels);
+
+  int n_epochs = 10;
+  int n_minibatchs = 10;
+
+  // run train
+  net.train(train_inputs, train_labels, n_epochs, n_minibatchs, sgd);
 }
--- tino/core/optimizers/optimizer_helpers.h
+++ tino/core/optimizers/optimizer_helpers.h
@@ -5,35 +5,37 @@
 
 namespace tino {
   namespace core {
-    /*
-    template <typename T = TINO_FLOAT_TYPE, typename U>
-    std::unique_ptr<optimizers::abstract_optimizer_<T>> sgd(U alpha = 0.1) {
-      TINO_MAYBE_UNUSED(alpha);
-      return std::make_unique<optimizers::sgd_optimizer_<T>>(alpha);
-    }
-    */
-
-    template <typename T>
-    class SGD_;
-
-    template <typename T = TINO_FLOAT_TYPE>
-    class SGD_ {
-     private:
-      T _alpha;
-
-     public:
-      SGD_(const T alpha = 0.1)
-      : _alpha(alpha) {}
-
-      SGD_& alpha(const T alpha) {
-        _alpha = alpha;
-        return *this;
+    namespace optimizers {
+      /*
+      template <typename T = TINO_FLOAT_TYPE, typename U>
+      std::unique_ptr<optimizers::abstract_optimizer_<T>> sgd(U alpha = 0.1) {
+        TINO_MAYBE_UNUSED(alpha);
+        return std::make_unique<optimizers::sgd_optimizer_<T>>(alpha);
       }
+      */
 
-      std::unique_ptr<optimizers::abstract_optimizer_<T>> operator()() {
-        return std::make_unique<optimizers::sgd_optimizer_<T>>(_alpha);
-      }
-    };
-    using SGD = SGD_<TINO_FLOAT_TYPE>;
-  } // namespace core
+      template <typename T>
+      class SGD_;
+
+      template <typename T = TINO_FLOAT_TYPE>
+      class SGD_ {
+       private:
+        T _alpha;
+
+       public:
+        SGD_(const T alpha = 0.1)
+        : _alpha(alpha) {}
+
+        SGD_& alpha(const T alpha) {
+          _alpha = alpha;
+          return *this;
+        }
+
+        std::unique_ptr<optimizers::abstract_optimizer_<T>> operator()() const {
+          return std::make_unique<optimizers::sgd_optimizer_<T>>(_alpha);
+        }
+      };
+      using SGD = SGD_<TINO_FLOAT_TYPE>;
+    } // namespace optimizers
+  }   // namespace core
 } // namespace tino
